f2587827c358bb863211e0bf84805e6adf7265bd
hjjang <hjjang0207@yellostory.co.kr>
2019-06-17 18:12:25 +0900

    #2.8 Express Core: Routing

--- app.js
+++ app.js
@@ -3,19 +3,20 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser"; // 이후 세션 데이터 저장등 쿠키 에 사용
 import bodyParser from "body-parser"; // 넘어온 데이터 처리용
+import {
+  userRouter
+} from "./router";
+
 const app = express();
 
 const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // json 타입 지정
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -26,4 +27,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter); // /user에 대한 하위 라우터를 생성
+
+export default app;
