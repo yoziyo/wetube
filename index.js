e40274c4601b75e4b39efa983c92324647151a5e
hjjang <hjjang0207@yellostory.co.kr>
2019-06-17 17:42:12 +0900

    #2.7 Express Core: Middlewares part Two

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser"; // 이후 세션 데이터 저장등 쿠키 에 사용
+import bodyParser from "body-parser"; // 넘어온 데이터 처리용
 const app = express();
 
 const PORT = 4000;
@@ -10,12 +14,13 @@ const handleHome = (req, res) => res.send("Hello from ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
-const betweenHome = (req, res, next) => {
-  console.log("Between");
-  next();
-}
-
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
