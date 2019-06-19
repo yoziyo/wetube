cd80cc965484c7b3e646696cc888eb803a417b9a
hjjang <hjjang0207@yellostory.co.kr>
2019-06-19 18:00:24 +0900

    #2.16 Local Variables in Pug

--- app.js
+++ app.js
@@ -3,24 +3,29 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser"; // 이후 세션 데이터 저장등 쿠키 에 사용
 import bodyParser from "body-parser"; // 넘어온 데이터 처리용
+import {
+  localsMiddleware
+} from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
 const app = express();
 
-// view 엔진으로 pug 설정
-// 기본적으로 /views 경로를 사용함
-app.set("view engine", "pug");
-
 // 미들웨어 사용
+app.use(helmet());
 app.use(cookieParser());
 app.use(bodyParser.json()); // json 타입 지정
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
+
+// view 엔진으로 pug 설정
+// 기본적으로 /views 경로를 사용함
+app.set("view engine", "pug");
 
 // 라우터 지정
 app.use(routes.home, globalRouter);
