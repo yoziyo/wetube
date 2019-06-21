73cfcfee22cff3191ffc2992297b00a9b09ab58b
yozi <j16412000kr@gmail.com>
2019-06-21 20:44:04 +0900

    #2.21 Home Controller

--- init.js
+++ init.js
@@ -3,7 +3,7 @@ import app from "./app";
 const PORT = 4000;
 
 const handleListening = () => {
-    console.log(`🔥 Listening on http://localhost:${PORT}`);
-}
+  console.log(`🔥 Listening on http://localhost:${PORT}`);
+};
 
 app.listen(PORT, handleListening);
