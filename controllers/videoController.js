20012ca4c454260adbbbbf6a1bc24ef31864584b
hjjang <hjjang0207@yellostory.co.kr>
2019-06-21 16:23:31 +0900

    #2.18 Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,27 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const home = (req, res) => res.render("home", {
+    pageTitle: 'Home'
+});
+export const search = (req, res) => {
+    const {
+        query: {
+            term: searchingBy
+        }
+    } = req;
+
+    res.render("search", {
+        pageTitle: 'Search',
+        searchingBy
+    });
+};
+export const upload = (req, res) => res.render("upload", {
+    pageTitle: 'Upload'
+});
+export const videoDetail = (req, res) => res.render("videoDetail", {
+    pageTitle: "video detail"
+});
+export const editVideo = (req, res) => res.render("editVideo", {
+    pageTitle: "video edit"
+});
+export const deleteVideo = (req, res) => res.render("deleteVideo", {
+    pageTitle: "video delete"
+});
