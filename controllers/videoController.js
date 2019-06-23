68958fe2b755b79d207f343580420d13f57b7bdf
yozi <j16412000kr@gmail.com>
2019-06-23 16:23:35 +0900

    #2.22 Home Controller part Two

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,27 +1,33 @@
-export const home = (req, res) => res.render("home", {
-    pageTitle: 'Home'
-});
-export const search = (req, res) => {
-    const {
-        query: {
-            term: searchingBy
-        }
-    } = req;
+import { videos } from "../db";
 
-    res.render("search", {
-        pageTitle: 'Search',
-        searchingBy
-    });
+export const home = (req, res) =>
+  res.render("home", {
+    pageTitle: "Home",
+    videos
+  });
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+
+  res.render("search", {
+    pageTitle: "Search",
+    searchingBy
+  });
 };
-export const upload = (req, res) => res.render("upload", {
-    pageTitle: 'Upload'
-});
-export const videoDetail = (req, res) => res.render("videoDetail", {
+export const upload = (req, res) =>
+  res.render("upload", {
+    pageTitle: "Upload"
+  });
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", {
     pageTitle: "video detail"
-});
-export const editVideo = (req, res) => res.render("editVideo", {
+  });
+export const editVideo = (req, res) =>
+  res.render("editVideo", {
     pageTitle: "video edit"
-});
-export const deleteVideo = (req, res) => res.render("deleteVideo", {
+  });
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", {
     pageTitle: "video delete"
-});
+  });
