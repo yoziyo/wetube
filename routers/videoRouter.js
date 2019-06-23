6bd58416fb59a3b52e270165a3eff5417405a840
yozi <j16412000kr@gmail.com>
2019-06-23 17:05:25 +0900

    #2.24 Log In and User Profile Controller

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,16 +1,16 @@
 import express from "express";
 import routes from "../routes";
 import {
-    upload,
-    videoDetail,
-    deleteVideo,
-    editVideo
+  upload,
+  videoDetail,
+  deleteVideo,
+  editVideo
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
